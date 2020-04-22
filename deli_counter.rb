42ad022dc7cf5002f097e5d3f9958caed92e4a9a
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-22 04:49:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,8 +26,7 @@ end
 # end
 
 def take_a_number(katz_deli, name)
-katz_deli = []
-  katz_deli << name
+katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
