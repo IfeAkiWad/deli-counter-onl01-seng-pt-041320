3871878014716bbeb292ca034099d6aad3153db8
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-22 05:11:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -34,7 +34,8 @@ def now_serving(name)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
   elsif katz.length >= 1
+    puts "Currently serving #{katz_deli[0]}."
     katz_deli.shift()
-      puts "Currently serving #{katz_deli[0]}."
+
   end
 end
