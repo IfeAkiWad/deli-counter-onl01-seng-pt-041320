ab469e460fe2e8493d86193fc0ab06b4d46f1b08
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-22 03:45:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,9 @@ def line(katz_deli)
   if katz_deli.length == 0
    puts "The line is currently empty."
  elsif katz_deli.length >= 1
-   puts "The line is currently:"
+   new_array = []
+   counter = 1
+
 
 
 
