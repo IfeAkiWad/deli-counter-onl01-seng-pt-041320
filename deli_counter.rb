e007150112c1213c6b4ded47c55de19ab7385561
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-22 04:31:48 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,9 +16,9 @@ end
 
 def take_a_number(katz_deli, name)
 katz_deli = []
-counter =
-  if condition
-    katz_deli.each do
+counter = 1
+  if katz_deli == 0
+    katz_deli.each do |name|
     counter += 1  
     end
     puts "Welcome, #{name}. You are number #{counter} in line."
