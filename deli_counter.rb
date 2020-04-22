01d023764e900b1d797d1afa2bb4aa6d7f280ba3
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-22 04:13:57 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
    counter = 1
    katz_deli.each do |name|
      new_array << ("#{counter}. #{name}")
-     counter =+ 1
+     counter += 1
    end
    puts "The line is currently: #{new_array.join(" ")}"
   end
