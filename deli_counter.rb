deb6082eea450a67b48e9dbc40011d616beadfd3
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-22 05:09:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -35,6 +35,6 @@ def now_serving(name)
     puts "There is nobody waiting to be served!"
   elsif katz.length >= 1
     katz_deli.shift << katz_deli[0]
-      puts "Currently serving #{katz_deli[0]}."
+      puts "Currently serving #{name}."
   end
 end
